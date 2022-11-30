ccac29bc465f661e1585c539422d15d9c023d6c1
Vincent Toon <vincenttoon22@gmail.com>
2022-11-30 00:20:44 -0600

    base resume added

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,6 +3,45 @@ import React from "react";
 function Resume() {
   return (
     <div>
+      <section>
+        <p>Resume Link Here</p>
+      </section>
+      <section>
+        <div>
+        <h3>Primary Practices:</h3>
+        <ul>
+          <li>Mongoose</li>
+          <li>Express</li>
+          <li>React</li>
+          <li>Node.js</li>
+        </ul>
+        </div>
+        <div>
+        <h3>Technologies:</h3>
+        <ul>
+          <li>Javascript</li>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>Progressive Web Applications</li>
+          <li>Bootstrap</li>
+          <li>Bulma</li>
+          <li>API's</li>
+          <li>Sequelize</li>
+          <li>NoSQL</li>
+          <li>SQL</li>
+        </ul>
+        </div>
+        <div>
+          <h3>Logical Practices:</h3>
+        <ul>
+          <li>State</li>
+          <li>OOP</li>
+          <li>Object Relational Mapping</li>
+          <li>Model View Controller</li>
+          <li>Computer Science</li>
+        </ul>
+        </div>
+      </section>
     </div>
   );
 }
