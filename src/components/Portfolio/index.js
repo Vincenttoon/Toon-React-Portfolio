199709b1f5ef0692a65d2336d2b6978133c9e8a4
Vincent Toon <vincenttoon22@gmail.com>
2022-11-30 11:53:45 -0600

    projects added:

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,85 @@
 import React from "react";
+import breweryFinderImg from "../../assets/small/post-search.png";
+import employeeImg from "../../assets/small/employee.jpg";
+import jateImg from "../../assets/small/jate-header-img.jpg";
+import ecomImg from "../../assets/small/ecom-img.jpg";
+import rise2ChallengeImg from "../../assets/small/rise2challenge.jpg";
+import teamBuildingImg from "../../assets/small/Results.jpg";
 
 function Portfolio() {
   return (
     <div>
-      <p>This is the Portfolio tab</p>
+      <section>
+        <div>
+          <h2>Projects:</h2>
+        </div>
+        <div className="project">
+          <div className="project-img">
+            <a href="https://github.com/Vincenttoon/brewery-finder">
+                {" "}
+              <img src={breweryFinderImg} className="port-img" style={{ width: "40%" }} alt='brewery finder home screen'/>
+            </a>
+          </div>
+          <div className="project-description">
+            <a href='https://vincenttoon.github.io/brewery-finder/'><h4>Find Your Brew</h4></a>
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-img">
+            <a href="https://github.com/Vincenttoon/textfas-burrito-two">
+                {" "}
+              <img src={jateImg} className="port-img" style={{ width: "40%" }} alt='brewery finder home screen'/>
+            </a>
+          </div>
+          <div className="project-description">
+            <a href='https://safe-atoll-66410.herokuapp.com/'><h4>Just Another Text Editor</h4></a>
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-img">
+            <a href="https://github.com/Vincenttoon/ecommerce-back-end">
+                {" "}
+              <img src={ecomImg} className="port-img" style={{ width: "40%" }} alt='brewery finder home screen'/>
+            </a>
+          </div>
+          <div className="project-description">
+            <a href='https://github.com/Vincenttoon/ecommerce-back-end'><h4>Ecommerce Back End</h4></a>
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-img">
+            <a href="https://github.com/JayWhiteBuffalo/Rise2Challenge">
+                {" "}
+              <img src={rise2ChallengeImg} className="port-img" style={{ width: "40%" }} alt='brewery finder home screen'/>
+            </a>
+          </div>
+          <div className="project-description">
+            <a href='https://thawing-atoll-91539.herokuapp.com/'><h4>Rise2Challenge</h4></a>
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-img">
+            <a href="https://github.com/Vincenttoon/employee-tracker-extraordinaire">
+                {" "}
+              <img src={employeeImg} className="port-img" style={{ width: "40%" }} alt='brewery finder home screen'/>
+            </a>
+          </div>
+          <div className="project-description">
+            <a href='https://github.com/Vincenttoon/employee-tracker-extraordinaire'><h4>Employee Tracker</h4></a>
+          </div>
+        </div>
+        <div className="project">
+          <div className="project-img">
+            <a href="https://github.com/Vincenttoon/team-building-exercise">
+                {" "}
+              <img src={teamBuildingImg} className="port-img" style={{ width: "40%" }} alt='team building page'/>
+            </a>
+          </div>
+          <div className="project-description">
+            <a href='https://github.com/Vincenttoon/team-building-exercise'><h4>Team Builder</h4></a>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -8,38 +8,38 @@ function Resume() {
       </section>
       <section>
         <div>
-        <h3>Primary Practices:</h3>
-        <ul>
-          <li>Mongoose</li>
-          <li>Express</li>
-          <li>React</li>
-          <li>Node.js</li>
-        </ul>
+          <h3>Primary Practices:</h3>
+          <ul>
+            <li>Mongoose</li>
+            <li>Express</li>
+            <li>React</li>
+            <li>Node.js</li>
+          </ul>
         </div>
         <div>
-        <h3>Technologies:</h3>
-        <ul>
-          <li>Javascript</li>
-          <li>HTML</li>
-          <li>CSS</li>
-          <li>Progressive Web Applications</li>
-          <li>Bootstrap</li>
-          <li>Bulma</li>
-          <li>API's</li>
-          <li>Sequelize</li>
-          <li>NoSQL</li>
-          <li>SQL</li>
-        </ul>
+          <h3>Technologies:</h3>
+          <ul>
+            <li>Javascript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Progressive Web Applications</li>
+            <li>Bootstrap</li>
+            <li>Bulma</li>
+            <li>API's</li>
+            <li>Sequelize</li>
+            <li>NoSQL</li>
+            <li>SQL</li>
+          </ul>
         </div>
         <div>
           <h3>Logical Practices:</h3>
-        <ul>
-          <li>State</li>
-          <li>OOP</li>
-          <li>Object Relational Mapping</li>
-          <li>Model View Controller</li>
-          <li>Computer Science</li>
-        </ul>
+          <ul>
+            <li>State</li>
+            <li>OOP</li>
+            <li>Object Relational Mapping</li>
+            <li>Model View Controller</li>
+            <li>Computer Science</li>
+          </ul>
         </div>
       </section>
     </div>
